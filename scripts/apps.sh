44d5be694a65d0d67e251796c16253fb80aded48
WALLFLOWERYWOLF <121654472+WALLFLOWERYWOLF@users.noreply.github.com>
2024-06-05 16:02:09 +0530

    feat: add apps

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -2,4 +2,4 @@
 
 #installing apps and shell
 echo "Installing Apps and Shell"
-paru -S --noconfirm ntfs-3g btrfs-assistant snap-pac grub-btrfs snapper-support zsh neovim wezterm curl tmux hyprland hyprpaper hyprlock waybar wofi dolphin npm pnpm yt-dlp ytfzf google-chrome xdman telegram-desktop podman distrobox boxbuddy virt-manager
+paru -S --noconfirm ntfs-3g btrfs-assistant snapper-support snap-pac inotify-tools grub-btrfs snapper-rollback zsh neovim wezterm curl tmux hyprland hyprpaper hyprlock waybar wofi dolphin npm pnpm yt-dlp ytfzf youtube-dl google-chrome xdman telegram-desktop podman distrobox boxbuddy virt-manager
