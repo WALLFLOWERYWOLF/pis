90ab1b41460937696d7b35c90dd7d844531a50c6
WALLFLOWERYWOLF <121654472+WALLFLOWERYWOLF@users.noreply.github.com>
2024-06-16 09:17:28 +0530

    feat: add apps

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,5 +1,65 @@
 #!/bin/env bash
 
+apps=(
+	ntfs-3g
+	btrfsmaintenance
+	snapper
+	btrfs-assistant
+	snap-pac
+	inotify-tools
+	grub-btrfs
+	zsh
+	neovim
+	wezterm
+	curl
+	tmux
+	hyprland
+	hyprpaper
+	hyprlock
+	waybar
+	wofi
+	dolphin
+	npm
+	pnpm
+	yt-dlp
+	ytfzf
+	youtube-dl
+	xdman
+	telegram-desktop
+	discord
+	podman
+	distrobox
+	boxbuddy
+	qemu-full
+	virt-manager
+	cachyos-gaming-meta
+	retroarch
+	mpv
+	qbittorrent
+	thunderbird
+	obs-studio
+	syncplay
+	pamac
+	droidcam
+	inkscape
+	krita
+	darktable
+	libreoffice-fresh
+	aisleroit
+	blender
+	joplin
+	obsidian
+	celluloid
+	discover
+	k3b
+	firefox
+	flatpak
+)
+
+flatpaks=(
+	net.pcsx2.PCSX2
+)
+
 #installing apps and shell
 echo "Installing Apps and Shell"
-paru -S --noconfirm ntfs-3g btrfs-assistant snapper snapper-support snap-pac inotify-tools grub-btrfs zsh neovim wezterm curl tmux hyprland hyprpaper hyprlock waybar wofi dolphin sshfs npm pnpm yt-dlp ytfzf youtube-dl google-chrome xdman telegram-desktop discord podman distrobox boxbuddy virt-manager cachyos-gaming-meta retroarch mpv
+paru -S --noconfirm ${apps[*]}
