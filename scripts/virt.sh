c00747493a6cd5c7c1a776244816f653d3b8c2ad
WALLFLOWERYWOLF <121654472+WALLFLOWERYWOLF@users.noreply.github.com>
2024-06-18 16:48:24 +0530

    fix: proper redirection of root priviledges

--- scripts/virt.sh
+++ scripts/virt.sh
@@ -2,12 +2,12 @@
 
 sudo sed -i '/unix_sock_group = "libvirt"/s/^#//' /etc/libvirt/libvirtd.conf
 sudo sed -i '/unix_sock_rw_perms = "0770"/s/^#//' /etc/libvirt/libvirtd.conf
-sudo echo 'log_filters="3:qemu 1:libvirt"' >> /etc/libvirt/libvirtd.conf
-sudo echo 'log_outputs="2:file:/var/log/libvirt/libvirtd.log"' >> /etc/libvirt/libvirtd.conf
+echo 'log_filters="3:qemu 1:libvirt"' | sudo tee -a /etc/libvirt/libvirtd.conf
+echo 'log_outputs="2:file:/var/log/libvirt/libvirtd.log"' | sudo tee -a /etc/libvirt/libvirtd.conf
 sudo usermod -a -G kvm,libvirt $(whoami)
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
-sudo echo 'user = "shantanu"'
-sudo echo 'group = "shantanu"'
+echo 'user = "shantanu"' | sudo tee -a /etc/libvirt/libvirtd.conf
+echo 'group = "shantanu"' | sudo tee -a /etc/libvirt/libvirtd.conf
 sudo systemctl restart libvirtd
 sudo virsh net-autostart default
